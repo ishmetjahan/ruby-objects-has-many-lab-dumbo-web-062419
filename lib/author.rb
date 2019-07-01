af677eb7485c28fb4de09937f4165515e5c45cdd
Ishmet <ishmetjahan@yahoo.com>
2019-07-01 02:29:13 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,13 @@
+class Author 
+  
+  attr_accessor :name, :posts
+  
+  @@all = []
+  
+  def initialize (name)
+    @name = name
+
+  end
+  
+  
+end
---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+class Song 
+  
+attr_accessor :name, :artist
+ 
+  @@all = []
+  
+  def initialize (name)
+  @name = name
+    
+  end
+  
+  def artist_name (artist_name)
+    @artist = artist_name
+    
+  end
+
+
+  
+end
